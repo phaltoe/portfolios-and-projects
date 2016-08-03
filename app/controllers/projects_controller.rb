cc00e2e55e04bb4a383b7b246eaef0711f8f86df
phaltoe <reverse9@live.com>
2016-08-03 13:29:57 -0300

    edit form added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,40 @@
 class ProjectsController < ApplicationController
+  before_action :set_portfolio, only: [:new, :create, :edit, :show, :update]
 
   def new
-    @portfolio = Portfolio.find(params[:portfolio_id])
     @project = @portfolio.projects.build
   end
 
   def create
+    @project = @portfolio.projects.build(project_params)
+
+    if @project.save
+      redirect_to @portfolio
+    else
+      render new_portfolio_project_path
+    end
+  end
+
+  def show
+    @project = @portfolio.projects.find(params[:id])
+  end
+
+  def edit
+    @project = @portfolio.projects.find(params[:id])
+  end
+
+  def update
+    @project = @portfolio.projects.find(params[:id])
+    @project.update(project_params)
+    redirect_to @portfolio
+  end
+  private
+
+  def set_portfolio
+    @portfolio = Portfolio.find(params[:portfolio_id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description, :portfolio_id)
   end
 end
